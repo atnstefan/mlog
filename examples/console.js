f378feef1088e550028dda14b0d957f25a105af9
Stefan Negrea <snegrea@gmail.com>
2014-04-23 22:34:04 -0500

    Initial commit

---
+++ examples/console.js
@@ -0,0 +1,21 @@
+// The MIT License (MIT)
+// Copyright (c) 2014 Stefan Negrea
+//
+// Use of this source code is governed by a MIT license that can be 
+// found in the LICENSE file.
+
+var logger = require('../lib/logger');
+
+
+var logConfiguration = {
+  "level": "INFO",
+  "loggers": {
+      "console": {
+    }
+  }
+};
+
+var log = new logger(logConfiguration);
+
+log.info('Test info log');
+log.error('Test error log with stack trace');
