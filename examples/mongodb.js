2fa74b0e8400afe258c57e82b29862a860da4d1b
Stefan Negrea <snegrea@gmail.com>
2016-01-14 14:36:29 -0600

    Update mongodb usage example.

--- examples/mongodb.js
+++ examples/mongodb.js
@@ -20,18 +20,18 @@ var log = new logger(logConfiguration);
 //create logs
 setInterval(function() {
   log.info('Test info log');
-}, 1000);
+}, 2000);
 
 setInterval(function() {
   log.error('Test error log with stack trace');
-}, 1000);
+}, 4000);
 
 //sample query from database
 //this will use the mongodb connection already established by the logger
 setInterval(function() {
   var mongodbLogger = log.getLoggers().mongodb;
-  mongodbLogger.model().find().exec()
+  mongodbLogger.model().find().sort({timestamp: -1}).limit(2).exec()
   .then(function(results){
     console.log(results);
   });
-}, 2000);
+}, 6000);
